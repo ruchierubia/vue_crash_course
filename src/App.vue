86792eb2fed314c99575177a0fb5d9d0f202dfbe
Ruchie V. Rubia <rrubia@fullscale.io>
2020-07-01 03:49:09 +0800

    Add props to a component and implement v-bind and v-for directive to show an array of the props

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Tasks/>
+    <Tasks v-bind:tasks="tasks"/>
   </div>
 </template>
 
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
+        <div v-bind:key ="task.id" v-for="task in tasks">
+            <h3>{{task.title}}</h3>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Tasks'
+    name: 'Tasks',
+    props: ['tasks']
 }
 </script>
 
