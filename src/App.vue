030c915b6678ee56518e0c5edc4960e7eac81518
Ruchie V. Rubia <rrubia@fullscale.io>
2020-07-01 03:39:03 +0800

    Add Tasks component and embed it to the main component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
   <div id="app">
+    <Tasks/>
   </div>
 </template>
 
 <script>
+import Tasks from './components/Tasks.vue';
+
   export default {
     name: 'app',
     components: {
-
-    }  
+      Tasks
+    },
+    data() {
+      return {
+        tasks: [
+          {
+            id: 1,
+            title: 'Task One',
+            completed: false
+          },
+          {
+            id: 2,
+            title: 'Task Two',
+            completed: false
+          },
+          {
+            id: 3,
+            title: 'Task Three',
+            completed: false
+          }
+        ]
+      }
+    } 
   }
 </script>
 
---
+++ src/components/Tasks.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Tasks'
+}
+</script>
+
+<style scoped>
+
+</style>
