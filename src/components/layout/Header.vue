e6eb15da64a1be6f4f46659b3f05d24f4eb59c0d
Ruchie V. Rubia <rrubia@fullscale.io>
2020-07-01 04:49:22 +0800

    Add header component

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="header">
+    <h1>Task List</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    .header {
+        background: #3333;
+        color: #fff;
+        text-align: center;
+        padding: 10px;
+    }
+    .header a {
+        color: #ffff;
+        padding-right: 5px;
+    }
+
+</style>
